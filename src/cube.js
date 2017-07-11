08820ede600182832688803ce7e334c7df3e5d17
Naoto Ikuno <moon_in_france@yahoo.co.jp>
2017-07-11 09:00:44 +0900

    src/cube.js was broken

--- src/cube.js
+++ src/cube.js
@@ -18,17 +18,10 @@
 //   B
 'use strict';
 const copy = arr => arr.concat();
-const baseRotate = trans => {
-    return face => {
-        const beforeFace = copy(face);
-        const result = copy(face);
-        for (let i = 0, _i = trans.length; i < _i; i++) result[i] = trans[i];
-        return result;
-    }
-};
-const rotateRight = baseRotate([6, 3, 0, 7, 1, 8, 5, 2]);
-const rotateLeft = baseRotate([2, 5, 8, 1, 7, 0, 3, 6]);
-const rotateRight2 = baseRotate([8, 7, 6, 5, 3, 2, 1, 0]);
+const baseRotate = trans => face => trans.map(x => face[x]);
+const rotateRight = baseRotate([6, 3, 0, 7, 4, 1, 8, 5, 2]);
+const rotateLeft = baseRotate([2, 5, 8, 1, 4, 7, 0, 3, 6]);
+const rotateRight2 = baseRotate([8, 7, 6, 5, 4, 3, 2, 1, 0]);
 const rotateLeft2 = rotateRight2;
 const setBase = (face, basePoint) => {
     // basePoint represent the index of face
@@ -43,18 +36,19 @@ const setBase = (face, basePoint) => {
 class Cube {
     constructor() {
         this.face = {
+            U: ['W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W'], // white
+            F: ['R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R'], // red
+            R: ['Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y'], // yellow
+            L: ['G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G'], // green
+            B: ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'], // orange
+            D: ['B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B'] // blue
         };
-        this.face.U = ['W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W']; // white
-        this.face.F = ['R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R']; // red
-        this.face.R = ['Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y']; // yellow
-        this.face.L = ['G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G']; // green
-        this.face.B = ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O']; // orange
-        this.face.D = ['B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B']; // blue
     }
     rotate(d) {
         const hasPrime = d.includes("'");
         const move = (hasPrime ? d.replace("'", '') : d)
             .replace(/\(([ruf])(2?)\)/, (_,a,two) => (a === 'r' ? 'x' : a === 'u' ? 'y' : 'z')+two); // d.slice(0, -1) cannot remove prime from "(r')".
+        const isCubeRotation = 'xyz'.includes(move);
         if (move === 'R') {
             const dir = ['U', 'F', 'D', 'B'];
             if (hasPrime) dir.reverse();
@@ -72,21 +66,20 @@ class Cube {
             this.face[dir[3]][8] = tmp[2];
 
             this.face.R = (!hasPrime ? rotateRight : rotateLeft)(this.face.R);
-        }
-        if ('LUDFB'.includes(move)) {
+        } else if ('LUDFB'.includes(move)) {
             const preMove = {
                 'L': ['(u2)', '(u2)'],
                 'U': ['(f)', "(f')"],
                 'D': ["(f')", '(f)'],
                 'F': ["(u')", '(u)'],
                 'B': ['(u)', "(u')"]
-            };
-            this.rotate(preMove[move][0]).rotate(!hasPrime ? 'R' : "R'").rotate(preMove[move][1]);
-        }
-        const isCubeRotation = 'xyz(r)(u)(f)'.contains(move);
-        if (isCubeRotation) {
+            }[move];
+            this.rotate(preMove[0])
+                .rotate(!hasPrime ? 'R' : "R'")
+                .rotate(preMove[1]);
+        } else if (isCubeRotation) {
             const dir = [...{x: 'UFDB', y: 'LFRB', z: 'ULDR'}[move]];
-            const R = {x: 'R', y: 'U', z: 'F'}[move], L = {x: 'L', y: 'D', z: 'B'}
+            const R = {x: 'R', y: 'U', z: 'F'}[move], L = {x: 'L', y: 'D', z: 'B'}[move];
             if (hasPrime) dir.reverse();
 
             const before = copy(this.face[dir[0]]);
@@ -94,21 +87,20 @@ class Cube {
             this.face[dir[3]] = before;
 
             this.face[R] = (!hasPrime ? rotateRight : rotateLeft)(this.face[R]);
-            this.face[L] = (!hasPrime ? rotateLett : rotateLeft)(this.face[L]]);
-        }
-        if (move === 'y') {
-            if (!hasPrime) {
-                this.face.R = setBase(this.face.R, 8);
-                this.face.B = setBase(this.face.B, 8);
-            } else {
-                this.face.B = setBase(this.face.B, 8);
-                this.face.L = setBase(this.face.L, 8);
+            this.face[L] = (!hasPrime ? rotateLeft : rotateRight)(this.face[L]);
+            if (move === 'y') {
+                if (!hasPrime) {
+                    this.face.R = setBase(this.face.R, 8);
+                    this.face.B = setBase(this.face.B, 8);
+                } else {
+                    this.face.B = setBase(this.face.B, 8);
+                    this.face.L = setBase(this.face.L, 8);
+                }
             }
-        }
-        if (move === 'z') {
-            for (let i = 0; i < 4; i++) this.face[dir[i]] = setBase(this.face[dir[i]], !hasPrime ? 6 : 2);
-        }
-        if (move === 'x2') {
+            if (move === 'z') {
+                for (let i = 0; i < 4; i++) this.face[dir[i]] = setBase(this.face[dir[i]], !hasPrime ? 6 : 2);
+            }
+        } else if (move === 'x2') {
             const beforeU = copy(this.face.U), beforeF = copy(this.face.F);
 
             this.face.U = copy(this.face.D);
@@ -118,8 +110,7 @@ class Cube {
 
             this.face.R = rotateRight2(this.face.R);
             this.face.L = rotateLeft2(this.face.L);
-        }
-        if (move === 'y2') {
+        } else if (move === 'y2') {
             const beforeF = copy(this.face.F), beforeR = copy(this.face.R);
 
             this.face.F = setBase(this.face.B, 8);
@@ -129,8 +120,7 @@ class Cube {
 
             this.face.U = rotateRight2(this.face.U);
             this.face.D = rotateLeft2(this.face.D);
-        }
-        if (move === 'z2') {
+        } else if (move === 'z2') {
             const beforeU = copy(this.face.U), beforeL = copy(this.face.L);
 
             this.face.U = copy(this.face.D);
@@ -145,7 +135,7 @@ class Cube {
     }
     copy() {
         const faces = {};
-        for (const key in this.face) {
+        for (var key of Object.keys(this.face)) {
             faces[key] = copy(this.face[key]);
         }
         const newCube = new Cube();
