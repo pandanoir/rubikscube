ac23e8a00ece4b2fade9378930d00c3f0625e54b
pandanoir <moon_in_france@yahoo.co.jp>
2020-09-18 23:38:53 +0900

    Divide Reorientation and Rotation into smaller types

--- src/type.ts
+++ src/type.ts
@@ -4,52 +4,48 @@ export type Face = 'U' | 'F' | 'R' | 'L' | 'B' | 'D';
 export type CubeType = {
   [key in Face]: CubeFace;
 };
-type Reorientation =
-  | 'x'
-  | 'y'
-  | 'z'
+export type BasicReorientation = 'x' | 'y' | 'z' | '(r)' | '(u)' | '(f)';
+export type SquaredReorientation =
   | 'x2'
   | 'y2'
   | 'z2'
+  | '(r2)'
+  | '(u2)'
+  | '(f2)';
+export type ReverseReorientation =
   | "x'"
   | "y'"
   | "z'"
-  | '(r)'
-  | '(u)'
-  | '(f)'
-  | '(r2)'
-  | '(u2)'
-  | '(f2)'
   | "(r')"
   | "(u')"
   | "(f')";
-type Rotation =
-  | 'R'
-  | 'L'
-  | 'U'
-  | 'D'
-  | 'F'
-  | 'B'
-  | "R'"
-  | "L'"
-  | "U'"
-  | "D'"
-  | "F'"
-  | "B'"
+type Reorientation = BasicReorientation | SquaredReorientation | ReverseReorientation;
+
+export type BasicRotation = 'R' | 'L' | 'U' | 'D' | 'F' | 'B' | 'M' | 'S' | 'E';
+export type SquaredRotation =
   | 'R2'
   | 'L2'
   | 'U2'
   | 'D2'
   | 'F2'
   | 'B2'
-  | 'M'
-  | 'S'
-  | 'E'
   | 'M2'
   | 'S2'
-  | 'E2'
+  | 'E2';
+export type ReverseRotation =
+  | "R'"
+  | "L'"
+  | "U'"
+  | "D'"
+  | "F'"
+  | "B'"
   | "M'"
   | "S'"
   | "E'";
+type Rotation =
+  | BasicRotation
+  | SquaredRotation
+  | ReverseRotation;
+
 export type Noop = '';
 export type Direction = Reorientation | Rotation | Noop;
