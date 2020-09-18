9ad8e0db1694069478f4c43bcdfa5c62ffaf44ca
pandanoir <moon_in_france@yahoo.co.jp>
2020-09-18 23:20:30 +0900

    Move reverseDirection into utils

--- src/cube.ts
+++ src/cube.ts
@@ -1,41 +1,5 @@
-import { copy } from './utils';
-
+import { copy, reverseDirection } from './utils';
 import { Direction, CubeFace, CubeType, Face, Color } from './type';
-
-//   B
-const reverseDirection = (dir: Direction): Direction => {
-  if (dir === 'R') return `R'`;
-  if (dir === 'L') return `L'`;
-  if (dir === 'U') return `U'`;
-  if (dir === 'D') return `D'`;
-  if (dir === 'F') return `F'`;
-  if (dir === 'B') return `B'`;
-
-  if (dir === `R'`) return 'R';
-  if (dir === `L'`) return 'L';
-  if (dir === `U'`) return 'U';
-  if (dir === `D'`) return 'D';
-  if (dir === `F'`) return 'F';
-  if (dir === `B'`) return 'B';
-
-  if (dir === 'M') return `M'`;
-  if (dir === 'S') return `S'`;
-  if (dir === 'E') return `E'`;
-
-  if (dir === `M'`) return 'M';
-  if (dir === `S'`) return 'S';
-  if (dir === `E'`) return 'E';
-
-  if (dir === 'x') return `x'`;
-  if (dir === 'y') return `y'`;
-  if (dir === 'z') return `z'`;
-
-  if (dir === `x'`) return 'x';
-  if (dir === `y'`) return 'y';
-  if (dir === `z'`) return 'z';
-  // M2とかR2
-  return dir;
-};
 
 const baseRotate: (transform: number[]) => (face: CubeFace) => CubeFace = (
   transform
--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,36 @@
+import { Direction } from "./type";
+
 export const copy = <T>(arr: T[]): T[] => arr.concat();
+export const reverseDirection = (dir: Direction): Direction => {
+    if (dir === 'R') return `R'`;
+    if (dir === 'L') return `L'`;
+    if (dir === 'U') return `U'`;
+    if (dir === 'D') return `D'`;
+    if (dir === 'F') return `F'`;
+    if (dir === 'B') return `B'`;
+  
+    if (dir === `R'`) return 'R';
+    if (dir === `L'`) return 'L';
+    if (dir === `U'`) return 'U';
+    if (dir === `D'`) return 'D';
+    if (dir === `F'`) return 'F';
+    if (dir === `B'`) return 'B';
+  
+    if (dir === 'M') return `M'`;
+    if (dir === 'S') return `S'`;
+    if (dir === 'E') return `E'`;
+  
+    if (dir === `M'`) return 'M';
+    if (dir === `S'`) return 'S';
+    if (dir === `E'`) return 'E';
+  
+    if (dir === 'x') return `x'`;
+    if (dir === 'y') return `y'`;
+    if (dir === 'z') return `z'`;
+  
+    if (dir === `x'`) return 'x';
+    if (dir === `y'`) return 'y';
+    if (dir === `z'`) return 'z';
+    // M2とかR2
+    return dir;
+  };
