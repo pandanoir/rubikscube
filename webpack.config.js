91670f5985cb46cb359455f7c62d781dd56372ad
pandanoir <moon_in_france@yahoo.co.jp>
2020-09-18 22:21:31 +0900

    Set libraryExport to 'default'

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,23 @@
-const resolve = path => require('path').resolve(__dirname, path);
-
 module.exports = {
-    mode: process.env.NODE_ENV || 'development',
-    entry: resolve('./src/cube.ts'),
-    output: {
-        filename: 'cube.js',
-        path: resolve('./dist'),
-        library: 'Cube',
-        libraryTarget: 'umd',
-        globalObject: 'this',
-    },
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-    module: {
-        rules: [
-            {test: /\.ts$/, use: 'ts-loader'},
-        ]
-    }
+  mode: process.env.NODE_ENV || 'development',
+  entry: require('path').resolve(__dirname, './src/cube.ts'),
+  output: {
+    filename: 'cube.js',
+    path: require('path').resolve(__dirname, './dist'),
+    library: 'Cube',
+    libraryExport: 'default',
+    libraryTarget: 'umd',
+    globalObject: 'this',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+      },
+    ],
+  },
 };
