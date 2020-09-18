d1d7ac760faa819ba98437cef453929fdff0a502
pandanoir <moon_in_france@yahoo.co.jp>
2020-09-18 23:50:38 +0900

    Define type guards

--- src/utils.ts
+++ src/utils.ts
@@ -1,39 +1,123 @@
-import { Direction, CubeFace } from "./type";
+import {
+  Direction,
+  CubeFace,
+  BasicRotation,
+  SquaredRotation,
+  ReverseRotation,
+  BasicReorientation,
+  SquaredReorientation,
+  ReverseReorientation,
+} from './type';
+
+const isBasicRotation = (dir: Direction): dir is BasicRotation => {
+  if (
+    dir === 'R' ||
+    dir === 'L' ||
+    dir === 'U' ||
+    dir === 'D' ||
+    dir === 'F' ||
+    dir === 'B' ||
+    dir === 'M' ||
+    dir === 'S' ||
+    dir === 'E'
+  )
+    return true;
+  return false;
+};
+const isBasicSquaredRotation = (dir: Direction): dir is SquaredRotation => {
+  if (
+    dir === 'R2' ||
+    dir === 'L2' ||
+    dir === 'U2' ||
+    dir === 'D2' ||
+    dir === 'F2' ||
+    dir === 'B2' ||
+    dir === 'M2' ||
+    dir === 'S2' ||
+    dir === 'E2'
+  )
+    return true;
+  return false;
+};
+const isBasicReverseRotation = (dir: Direction): dir is ReverseRotation => {
+  if (
+    dir === "R'" ||
+    dir === "L'" ||
+    dir === "U'" ||
+    dir === "D'" ||
+    dir === "F'" ||
+    dir === "B'" ||
+    dir === "M'" ||
+    dir === "S'" ||
+    dir === "E'"
+  )
+    return true;
+  return false;
+};
+const isBasicReorientation = (dir: Direction): dir is BasicReorientation => {
+  if (
+    dir === 'x' ||
+    dir === 'y' ||
+    dir === 'z' ||
+    dir === '(r)' ||
+    dir === '(u)' ||
+    dir === '(f)'
+  )
+    return true;
+  return false;
+};
+const isBasicSquaredReorientation = (
+  dir: Direction
+): dir is SquaredReorientation => {
+  if (
+    dir === 'x2' ||
+    dir === 'y2' ||
+    dir === 'z2' ||
+    dir === '(r2)' ||
+    dir === '(u2)' ||
+    dir === '(f2)'
+  )
+    return true;
+  return false;
+};
+const isBasicReverseReorientation = (
+  dir: Direction
+): dir is ReverseReorientation => {
+  if (
+    dir === "x'" ||
+    dir === "y'" ||
+    dir === "z'" ||
+    dir === "(r')" ||
+    dir === "(u')" ||
+    dir === "(f')"
+  )
+    return true;
+  return false;
+};
 
 export const copy = <T>(arr: T[]): T[] => arr.concat();
 export const reverseDirection = (dir: Direction): Direction => {
-    if (dir === 'R') return `R'`;
-    if (dir === 'L') return `L'`;
-    if (dir === 'U') return `U'`;
-    if (dir === 'D') return `D'`;
-    if (dir === 'F') return `F'`;
-    if (dir === 'B') return `B'`;
-  
-    if (dir === `R'`) return 'R';
-    if (dir === `L'`) return 'L';
-    if (dir === `U'`) return 'U';
-    if (dir === `D'`) return 'D';
-    if (dir === `F'`) return 'F';
-    if (dir === `B'`) return 'B';
-  
-    if (dir === 'M') return `M'`;
-    if (dir === 'S') return `S'`;
-    if (dir === 'E') return `E'`;
-  
-    if (dir === `M'`) return 'M';
-    if (dir === `S'`) return 'S';
-    if (dir === `E'`) return 'E';
-  
-    if (dir === 'x') return `x'`;
-    if (dir === 'y') return `y'`;
-    if (dir === 'z') return `z'`;
-  
-    if (dir === `x'`) return 'x';
-    if (dir === `y'`) return 'y';
-    if (dir === `z'`) return 'z';
-    // M2とかR2
-    return dir;
+  if (dir === '') return '';
+  // basic rotations
+  if (isBasicRotation(dir) || isBasicWideRotation(dir))
+    return `${dir}'` as Direction;
+  if (isBasicReverseRotation(dir)) return dir.replace(/'/, '') as Direction;
+  if (isBasicSquaredRotation(dir)) return dir;
+
+  if (isBasicSquaredReorientation(dir)) return dir;
+  if (isBasicReverseReorientation(dir))
+    return dir.replace(/'/, '') as Direction;
+
+  if (dir === 'x') return `x'`;
+  if (dir === 'y') return `y'`;
+  if (dir === 'z') return `z'`;
+
+  // advanced rotations
+  if (dir.includes('2')) return dir;
+  if (dir.includes("'")) return dir.replace(/'/, '') as Direction;
+  return `${dir}'` as Direction;
 };
+
 export const baseRotate = (transform: number[]) => (face: CubeFace) =>
   transform.map((x) => face[x]);
 
